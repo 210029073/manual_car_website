7eb9ea85f7596f624d922ff01b4d046cde7c5800
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-02-15 07:01:03 +0000

    Modified Model User.php for compatibility to migrations

--- manual/app/Models/User.php
+++ manual/app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'firstName',
+        'lastName',
         'email',
         'password',
+        'address',
+        'mobile'
     ];
 
     /**
