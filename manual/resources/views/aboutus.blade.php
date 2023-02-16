df47bb133bb5ee54ecb6dc9a512e6ca137411e2d
Ahsaan Kumar <ahsaankumar@outlook.de>
2023-02-16 23:44:58 +0000

    prototype Homepage

---
+++ manual/resources/views/aboutus.blade.php
@@ -0,0 +1,19 @@
+<head>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/homepage.css" type="text/css">
+    </head>
+    <body>
+        <div class="Navigation">
+    <a class="current" href="/homepage">Home</a>
+    <a href="/aboutus">About Us</a>
+    <a href="/contactus">Contact Us</a>
+</div>
+        </div><h1>About Us</h1></div>
+
+    </body>
+    </html>
+
