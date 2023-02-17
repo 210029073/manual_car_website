d04caa4a8194a92f6b30da4ec7e8e77be3284ccb
Ahsaan Kumar <ahsaankumar@outlook.de>
2023-02-17 04:03:45 +0000

    adjustments on all pages (overflow)

---
+++ manual/resources/views/contactus.blade.php
@@ -0,0 +1,20 @@
+<head>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/contactus.css" type="text/css">
+    </head>
+    <body>
+    <div class="Navigation">
+    <a class="current" href="/homepage">Home</a>
+    <div class="navpadding" ><a href="/products">Cars</a></div>
+    <div class="navpadding"><a href="/aboutus">About Us</a></div>
+   <div class="navpadding"> <a href="/contactus">Contact Us</a></div>
+   <div class="signup"> <a href="/signup">Sign Up</a>
+    <a href="/login">Login</div>
+</div>
+<div class="boxpush">
+    <div class="imagebox"></div></div>
+    
