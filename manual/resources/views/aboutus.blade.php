5f648ba8641b71dec9aa3e22b0b013584a27388a
Ahsaan Kumar <ahsaankumar@outlook.de>
2023-02-17 02:03:11 +0000

    About Us (prototype)

--- manual/resources/views/aboutus.blade.php
+++ manual/resources/views/aboutus.blade.php
@@ -4,16 +4,35 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/css/homepage.css" type="text/css">
+        <link rel="stylesheet" href="/css/aboutus.css" type="text/css">
     </head>
     <body>
-        <div class="Navigation">
+    <div class="Navigation">
     <a class="current" href="/homepage">Home</a>
-    <a href="/aboutus">About Us</a>
-    <a href="/contactus">Contact Us</a>
+    <div class="navpadding" ><a href="/products">Cars</a></div>
+    <div class="navpadding"><a href="/aboutus">About Us</a></div>
+   <div class="navpadding"> <a href="/contactus">Contact Us</a></div>
+   <div class="signup"> <a href="/signup">Sign Up</a>
+    <a href="/login">Login</div>
 </div>
-        </div><h1>About Us</h1></div>
+<div class="aboutus2">
+<div class="boxer2"></div></div>
 
+<div class="AboutUsText">
+    <h1>About Us</h1></div>
+
+<div class="AboutUs">
+
+      <div class="boxer">
+        <h1>Introduction</h1>
+        <h2>Manual is a digital business which is built to help customers to be able to afford well functioning cars for cheaper.</h2></div>
+ </div>  
+</div>
+       
+<div class="AboutUs1">
+      <div class="boxer1"><h1>Refunds</h1><h2>In order to buy from Manual, you will have to create an Account. After creating the Account, you will then log into that Account and browse Cars that we have available for you to buy.</h2></div>
+ </div>  
+</div>
     </body>
     </html>
 
