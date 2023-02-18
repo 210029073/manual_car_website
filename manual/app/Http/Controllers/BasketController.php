9d4ea1905f001a05e6afdeae8e3a9eb6281c368c
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-02-18 13:42:34 +0000

    Added php-ds/php-ds dependency re-run composer install, and created basket mechanism using cookies

---
+++ manual/app/Http/Controllers/BasketController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BasketController
+{
+
+
+    //created basket cookie do not remove! Ibrahim Ahmad
+//        $basket = new Vector();
+//        $product = new Product("550d", "BMW", "2015 BMW 5 Series 550d M Sport Diesel 5.0 litre engine",12565.34);
+//        $product1 = new Product("320d", "BMW", "2012 BMW 3 Series 320d Normal Diesel 3.0 litre engine",12565.34);
+//
+//        $basket->push($product);
+//        $basket->push($product1);
+
+//        dd($basket);
+//        $data = serialize($basket);
+//        setcookie(name: "basket", value: $data, expires_or_options:  time() + 2592000);
+//        $data1 = $_COOKIE['basket'];
+
+//        $data1 = unserialize($data1);
+//        dd($data1);
+
+}
--- manual/app/Http/Controllers/HomeController.php
+++ manual/app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Ramsey\Collection\Set;
+use Ds\Vector;
 
 class HomeController extends Controller
 {
@@ -12,17 +12,6 @@ class HomeController extends Controller
      * @return A new view holding the homepage
      */
     public function index() {
-        $set = new Set(Product::class);
-        $product = new Product("550d", "BMW", "2015 BMW 5 Series 550d M Sport Diesel 5.0 litre engine",12565.34);
-        $product1 = new Product("320d", "BMW", "2012 BMW 3 Series 320d Normal Diesel 3.0 litre engine",12565.34);
-
-        $set->add($product);
-        $set->add($product1);
-
-//        dd($set);
-        $value = cookie('I have a cookie', 7);
-
-//        dd($value);
         return view('homepage');
     }
 
