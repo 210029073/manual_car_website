9c56017e3e13a9d44818fd46efe83bb7112ecd47
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-02-19 06:41:33 +0000

    Added feature for adding an item

--- manual/app/Http/Controllers/BasketController.php
+++ manual/app/Http/Controllers/BasketController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Ds\Vector;
 
 /**
@@ -26,20 +27,48 @@ class BasketController extends Controller
      */
     public function createBasket() {
         //dummy products
-        $product = new Product("550d", "BMW", "2015 BMW 5 Series 550d M Sport Diesel 5.0 litre engine",12565.34);
-        $product1 = new Product("320d", "BMW", "2012 BMW 3 Series 320d Normal Diesel 3.0 litre engine",12565.34);
+//        $product = new Product("550d", "BMW", "2015 BMW 5 Series 550d M Sport Diesel 5.0 litre engine",12565.34);
+//        $product1 = new Product("320d", "BMW", "2012 BMW 3 Series 320d Normal Diesel 3.0 litre engine",12565.34);
 
-        $this->basket->push($product, $product1);
+//        $this->basket->push($product, $product1);
 
         //creates a new cookie that will expire in 30 days.
         setcookie("manualBasket", serialize($this->basket), time() + 2592000);
     }
 
     /**
+     * This will simply add an item to basket, as well as remembering
+     * the previous products.
+     *
+     * @since 19-02-2023
+     * @author Ibrahim Ahmad <210029073@aston.ac.uk>
+     * @version 1.0
+    */
+    public function addProductToBasket() {
+
+
+
+        //using eloquent ORM to retrieve an item
+        //constructs product object
+        //then pushes it to the basket
+        //then creates a new cookies to hold the new data along
+        //with the previous data
+        $data = Products::where("productsId", 16)->get()[0];
+        $product = new Product($data->model, $data->brand, $data->description,
+        $data->price);
+
+        $this->basket->push($product);
+
+        $this->createBasket();
+
+//        dd($this->basket->find($product));
+    }
+
+    /**
     * This will return the basket page, if a basket is empty a new basket will be created.
      * @author Ibrahim Ahmad <210029073@aston.ac.uk>
      * @since 18-02-2023
-     * @version 1.0
+     * @version 1.0.1
      */
     public function viewBasket() {
         //cookie will expire within 30 days.
@@ -60,7 +89,7 @@ class BasketController extends Controller
      * @version 1.0
     */
     private function destroyCookie() {
-        //destroys cookie such that it will point to the last 30 days.
+        //destroys cookie such that it will point to the last 6 seconds.
         setcookie("manualBasket", "", time() - 6);
     }
 
