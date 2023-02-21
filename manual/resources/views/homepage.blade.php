b720d2b7965f1341f80e3f1be06d3909a4f9cfaa
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-02-21 15:48:08 +0000

    Removed invalid CSS property and set values due to bad practices in CSS.

--- manual/resources/views/homepage.blade.php
+++ manual/resources/views/homepage.blade.php
@@ -1,32 +1,37 @@
+<html lang="en">
 <head>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/css/homepage.css" type="text/css">
-    </head>
-    <body>
-        <div class="Navigation">
-            <a class="current" href="/homepage">Home</a>
-            <div class="navpadding" ><a href="/products">Cars</a></div>
-            <div class="navpadding"><a href="/aboutus">About Us</a></div>
-            <div class="navpadding"> <a href="/contactus">Contact Us</a></div>
-            @if(\Illuminate\Support\Facades\Auth::check())
-                <div class="signup"><a href="{{route('logout')}}">Logout</a></div>
-            @else
-                <div class="signup"> <a href="{{route('signup')}}">Sign Up</a>
-                    <a href="{{route('loginPage')}}">Login</a>
-                </div>
-            @endif
-        </div>
-        <div class="body1">
-            <div class="WelcomeandBrowse">
-                <h1>Welcome to Manual</h1>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/homepage.css" type="text/css">
+</head>
+<body>
+<div class="Navigation">
+    <div class="Navigation Navigation-left">
+        <a class="current" href="/homepage">Home</a>
+    </div>
+    <div class="Navigation Navigation-center">
+        <div class="navpadding" ><a href="/products">Cars</a></div>
+        <div class="navpadding"><a href="/aboutus">About Us</a></div>
+        <div class="navpadding"> <a href="/contactus">Contact Us</a></div>
+    </div>
+    <div class="Navigation Navigation-right">
+        @if(\Illuminate\Support\Facades\Auth::check())
+            <div class="signup"><a href="{{route('logout')}}">Logout</a></div>
+        @else
+            <div class="signup"> <a href="{{route('signup')}}">Sign Up</a>
+                <a href="{{route('loginPage')}}">Login</a>
             </div>
-            </div>
-            <div class="browseCars"><h3>Browse Cars</h3></div>
-        </div>
-    </body>
+        @endif
+    </div>
+</div>
+<div class="body1">
+    <div class="WelcomeandBrowse">
+        <h1>Welcome to Manual</h1>
+    </div>
+</div>
+<div class="browseCars"><h3>Browse Cars</h3></div>
+</div>
+</body>
 </html>
 
