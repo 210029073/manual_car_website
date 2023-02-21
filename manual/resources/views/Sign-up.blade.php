e0042e451d243798940fc1ab4491e42b5f8ee031
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-02-21 16:30:55 +0000

    Fixed CSS for login and register page

--- manual/resources/views/Sign-up.blade.php
+++ manual/resources/views/Sign-up.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts')
+@section('content')
 <html>
     <head>
         <title>Register</title>
@@ -7,35 +9,63 @@
 
     <body>
         <div class="Sign-up">
-            <form method="post" action="{{route("register")}}">
-                @csrf
-                <label>First name</label>
-                <br/>
-                <input type="text" name="firstName" placeholder="Enter your first name"/>
-                <br/>
-                <label>Last name</label>
-                <br/>
-                <input type="text" name="lastName" placeholder="Enter your last name"/>
-                <br/>
-                <label>Email:</label>
-                <br>
-                <input type="text" name="email" placeholder="Enter your email"/>
-                <br/>
-                <label>Password</label>
-                <br/>
-                <input type="password" name="password" placeholder="Enter your password"/>
-                <br/>
-                <label>Address</label>
-                <br/>
-                <input type="text" name="address" placeholder="Enter your address"/>
-                <br/>
-                <label>Mobile Number</label>
-                <br/>
-                <input type="text" name="mobile" placeholder="Enter your mobile number"/>
-                <br/>
-                <br/>
-                <input type="submit" name="btnSubmit" value="Register">
-            </form>
+            <div class="ui-container">
+                <div class="ui-prompt-box">
+                    <div class="login-container">
+                        <form method="post" action="{{route("register")}}">
+                            @csrf
+                            <div class="signupForm">
+                            <div>
+                            <label>First name</label>
+                            <br/>
+                            <input type="text" name="firstName" placeholder="Enter your first name"/>
+                            <br/>
+                            </div>
+
+                            <div>
+                            <label>Last name</label>
+                            <br/>
+                            <input type="text" name="lastName" placeholder="Enter your last name"/>
+                            <br/>
+                            </div>
+
+                            <div>
+                            <label>Email:</label>
+                            <br>
+                            <input type="text" name="email" placeholder="Enter your email"/>
+                            <br/>
+                            </div>
+
+                            <div>
+                            <label>Password</label>
+                            <br/>
+                            <input type="password" name="password" placeholder="Enter your password"/>
+                            <br/>
+                            </div>
+
+                            <div>
+                            <label>Address</label>
+                            <br/>
+                            <input type="text" name="address" placeholder="Enter your address"/>
+                            <br/>
+                            </div>
+                            <div>
+                            <label>Mobile Number</label>
+                            <br/>
+                            <input type="text" name="mobile" placeholder="Enter your mobile number"/>
+                            <br/>
+                            <br/>
+                            </div>
+                            <div>
+                            <input type="submit" name="btnSubmit" value="Register">
+                            </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </body>
 </html>
+@endsection
