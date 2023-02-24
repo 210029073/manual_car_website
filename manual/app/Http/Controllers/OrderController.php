fe814d27643c0725e50f2bf101853e56e545f53c
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-02-24 15:34:17 +0000

    Added feature in OrderController from basket page in order to process items in checkout

--- manual/app/Http/Controllers/OrderController.php
+++ manual/app/Http/Controllers/OrderController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
     public function proceedCheckout() {
         $basket = new BasketController();
         $basketCollection = $basket->basket();
+//        dd(Auth::id());
         foreach ($basketCollection as $basket) {
-            dd($basketCollection);
+            $product = [
+                'userID' => Auth::id(),
+                'productsId' => $basket->getId(),
+                'price' => $basket->getPrice(),
+                'deliveryDate' => date("d/m/y H:i:s"),
+                'orderDate' => date("d/m/y H:i:s")
+            ];
+            DB::table('orders')->insert($product);
         }
-        return "Order Checkout Route";
+        return redirect()->intended("homepage")->with('successCheckout', "Successfully processed item in checkout");
     }
 }
--- manual/app/Models/Order.php
+++ manual/app/Models/Order.php
@@ -8,4 +8,6 @@ use \Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable = ['userId', 'productsId', 'price', 'deliveryDate', 'orderDate'];
+    protected $hidden = ['updated_at'];
 }
