4e9700d1814bb06f4431d132c32d65c1a2219403
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-02-27 10:30:32 +0000

    Added form validation to Sign up page

--- manual/resources/views/Sign-up.blade.php
+++ manual/resources/views/Sign-up.blade.php
@@ -1,73 +1,85 @@
 @extends('layouts')
 @section('content')
-<html>
+    <html>
     <head>
         <title>Register</title>
         <!-- mobile view -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="/css/Sign-up.css" rel="stylesheet" type="text/css" />
+        <link href="/css/Sign-up.css" rel="stylesheet" type="text/css"/>
     </head>
 
     <body>
-        <div class="Sign-up">
-            <div class="ui-container">
-                <div class="ui-prompt-box">
-                    <div class="login-container">
-                        <form method="post" action="{{route("register")}}">
-                            @csrf
-                            <div class="signupForm">
+    <div class="Sign-up">
+        @if($errors->any())
+            <div class="errors">
+                <h2>Could not register</h2>
+                <p>One or two fields have not been filled correctly.
+                Please amend the following:</p>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <div class="ui-container">
+            <div class="ui-prompt-box">
+                <div class="login-container">
+                    <form method="post" action="{{route("register")}}">
+                        @csrf
+                        <div class="signupForm">
                             <div class="Title">Sign-up</div>
                             <div>
-                            <label>First name</label>
-                            <br/>
-                            <input type="text" name="firstName" placeholder="Enter your first name"/>
-                            <br/>
+                                <label>First name</label>
+                                <br/>
+                                <input type="text" name="firstName" placeholder="Enter your first name"/>
+                                <br/>
                             </div>
 
                             <div>
-                            <label>Last name</label>
-                            <br/>
-                            <input type="text" name="lastName" placeholder="Enter your last name"/>
-                            <br/>
+                                <label>Last name</label>
+                                <br/>
+                                <input type="text" name="lastName" placeholder="Enter your last name"/>
+                                <br/>
                             </div>
 
                             <div>
-                            <label>Email:</label>
-                            <br>
-                            <input type="text" name="email" placeholder="Enter your email"/>
-                            <br/>
+                                <label>Email:</label>
+                                <br>
+                                <input type="text" name="email" placeholder="Enter your email"/>
+                                <br/>
                             </div>
 
                             <div>
-                            <label>Password</label>
-                            <br/>
-                            <input type="password" name="password" placeholder="Enter your password"/>
-                            <br/>
+                                <label>Password</label>
+                                <br/>
+                                <input type="password" name="password" placeholder="Enter your password"/>
+                                <br/>
                             </div>
 
                             <div>
-                            <label>Address</label>
-                            <br/>
-                            <input type="text" name="address" placeholder="Enter your address"/>
-                            <br/>
+                                <label>Address</label>
+                                <br/>
+                                <input type="text" name="address" placeholder="Enter your address"/>
+                                <br/>
                             </div>
                             <div>
-                            <label>Mobile Number</label>
-                            <br/>
-                            <input type="text" name="mobile" placeholder="Enter your mobile number"/>
-                            <br/>
-                            <br/>
+                                <label>Mobile Number</label>
+                                <br/>
+                                <input type="text" name="mobile" placeholder="Enter your mobile number"/>
+                                <br/>
+                                <br/>
                             </div>
                             <div>
-                            <input type="submit" name="btnSubmit" value="Register">
+                                <input type="submit" name="btnSubmit" value="Register">
                             </div>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-
         </div>
+
+    </div>
     </body>
-</html>
+    </html>
 @endsection
