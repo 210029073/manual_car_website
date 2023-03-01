ea9de1b35cf3dd3e76123e2642a8c360ae4ca898
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-03-01 10:48:21 +0000

    In 401.blade.php improved look and feel for the error page.

--- manual/resources/views/errors/401.blade.php
+++ manual/resources/views/errors/401.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts')
 @section('content')
-<h1>401: Access is Forbidden</h1>
-<h2>{{$exception->getMessage()}}</h2>
+    <div class="page-errors">
+        <div class="page-errors unauthorised-error-container">
+            <div class="page-errors unauthorised-error-header">
+                <h1>401 Unauthorised</h1>
+                <h1>You are not allowed to access this page.</h1>
+            </div>
+            <div class="page-errors unauthorised-error-content">
+                <h2>Reason: </h2>
+                <p>{{$exception->getMessage()}}</p>
+                <h2>What can I do?</h2>
+                <p>Try the following links below: </p>
+                <ul>
+                    <li>To login <a href="{{route('loginPage')}}">Click here.</a></li>
+                    <li>To return to the homepage <a href="{{route('home')}}">Click here</a></>
+                </ul>
+            </div>
+        </div>
+    </div>
 @endsection
