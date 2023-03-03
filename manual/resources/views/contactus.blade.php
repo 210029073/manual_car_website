a9ac6c03267bb3dd6e9488ba7de1de93bbe1dbd8
Ahsaan Kumar <ahsaankumar@outlook.de>
2023-03-03 13:26:23 +0000

    further Front-End improvements

--- manual/resources/views/contactus.blade.php
+++ manual/resources/views/contactus.blade.php
@@ -11,9 +11,9 @@
         <a class="current" href="/homepage">Home</a>
     </div>
     <div class="Navigation Navigation-center">
-        <div class="navpadding"><a href="/products">Cars</a></div>
+        <div class="navpadding" ><a href="/products">Cars</a></div>
         <div class="navpadding"><a href="/aboutus">About Us</a></div>
-        <div class="navpadding"><a href="/contactus">Contact Us</a></div>
+        <div class="navpadding"> <a href="/contactus">Contact Us</a></div>
         <div class="signup"><a href="{{route('basket')}}">Basket</a></div>
     </div>
     <div class="Navigation Navigation-right">
@@ -21,30 +21,41 @@
             <div class="signup"><a href="{{route('pastOrder')}}">Past Order</a></div>
             <div class="signup"><a href="{{route('logout')}}">Logout</a></div>
         @else
-            <div class="signup"><a href="{{route('signup')}}">Sign Up</a>
+            <div class="signup"> <a href="{{route('signup')}}">Sign Up</a>
                 <a href="{{route('loginPage')}}">Login</a>
             </div>
         @endif
     </div>
 </div>
-<div class="boxpush">
-    <div class="imagebox"></div>
+
+<div class="topbackimage">
+    <!-- <img src="/images/.png" position="left:50px;"> -->
+</div>
+<div class="AboutUsTEST">
+    <div class="boxer">
+        <div class="h4box"><h1>Contact us Via:</h1>
+        <h4>E-Mail: 200070104@aston.ac.uk</h4>
+        <h4>Number: +44 7474 142552</h4>
+        <h4>Location: Aston St, Birmingham B4 7ET </h4></div>
+        <div class="img-container">
+</div>
 </div>
 
-<div class="contactContainer"><h1>E-Mail:
-
-    </h1>
-    <h3>200070104@aston.ac.uk</h3></div>
+<div class="AboutUsTEST">
+<div class="boxer">
+        <div class="h4box"><h1>Contact us Via:</h1>
+        <h4>E-Mail: 200070104@aston.ac.uk</h4>
+        <h4>Number: +44 7474 142552</h4>
+        <h4>Location: Aston St, Birmingham B4 7ET </h4></div>
+        <div class="img-container">
+    <div class="boxer">
+        <div class="h4box"><h1>Contact us Via:</h1>
+        <h4>E-Mail: 200070104@aston.ac.uk</h4>
+        <h4>Number: +44 7474 142552</h4>
+        <h4>Location: Aston St, Birmingham B4 7ET </h4></div>
+        <div class="img-container">
+</div>
 </div>
 
-<div class="contactContainer1"><h1>Github:</h1>
-
-    <h3>https://github.com/210029073/manual_car_website</h3>
-</div>
-
-<div class="contactContainer2"><h1>Location:</h1>
-    <h3>Aston St, Birmingham B4 7ET</h3>
-</div>
 </body>
 </html>
-
