0ac4bb59e697b68286a48a26bf9891207e60153e
Jacques22FC <JacWinmillFC@gmail.com>
2023-03-03 04:53:59 +0000

    Basket CSS WIP gonna finish when I wake up in 2hrs

--- manual/resources/views/Basket.blade.php
+++ manual/resources/views/Basket.blade.php
@@ -10,6 +10,7 @@
     </head>
 
 <body>
+    <main class = "container">
     <h1> Basket </h1>
     <?php
         use App\Http\Controllers\BasketController;
@@ -31,29 +32,47 @@
             <input type="hidden" name="transmission" value="{{$item->getTransmission()}}"/>
             <input type="hidden" name="quantity" value="{{$item->getQuantity()}}"/>
 
-            <div class ="panel">
-                <div class ="basket">
-                    <p>Product No. {{$item->getId()}}</p>
-                    <p>Car Brand: {{$item->getBrand()}}</p>
-                    <p>Car Model: {{$item->getModel()}}</p>
-                    <p>Price: £{{number_format($item->getPrice(), 2)}}</p>
-                    <p>Engine Capacity: {{number_format($item->getEngineCapacity(), 1)}}</p>
-                    <p>Transmission Type: {{$item->getTransmission()}}</p>
-                    <p>Stock remaining: {{$item->getQuantity()}}</p>
-                    <?php $total += $item->getPrice() ?>
-                    <button type="submit">Remove</button>
+            <section class ="basket">
+
+                <div class ="item-basket">
+                    <h2>Order summary</h>
+                    <div class = "item-profile">
+                        <div class = "item-image"></div>
+
+                        <div class = "item-details">
+                            <h3>{{$item->getBrand()}} {{$item->getModel()}}</h>
+                            <div class="wrapper">
+                                <div class= "item-info">
+                                    <p>Product No. {{$item->getId()}}</p>
+                                    <p>Car Brand: {{$item->getBrand()}}</p>
+                                    <p>Car Model: {{$item->getModel()}}</p>
+                                    <p>Price: £{{number_format($item->getPrice(), 2)}}</p>
+                                    <p>Engine Capacity: {{number_format($item->getEngineCapacity(), 1)}}</p>
+                                    <p>Transmission Type: {{$item->getTransmission()}}</p>
+                                    <p>Stock remaining: {{$item->getQuantity()}}</p>
+                                    <?php $total += $item->getPrice() ?>
+                                </dev>
+                            </div>
+                            
+
+                        </div>
+                        <button type="submit">Remove</button>
+                    </div>
                 </div>
-            </div>
+            </section>
             <br/>
         </form>
     @endforeach
     @if(\Illuminate\Support\Facades\Auth::check())
-        <div>
+        <div class = "wrapper">
+            <div class= "amount">
             <p>Total: £{{number_format($total, 2)}}</p>
             <form action="/basket/checkout" method="get">
                 <button type="submit">Checkout</button>
             </form>
+            </div>
         </div>
+    </main>
     @endif
 </body>
 @endsection
