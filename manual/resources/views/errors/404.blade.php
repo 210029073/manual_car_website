3341bd75fd126fc9671c8f997ed6205bf06eb96f
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-03-07 08:53:44 +0000

    Added 404 Error page in website.

---
+++ manual/resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts')
+@section('content')
+    <div class="page-errors">
+        <div class="page-errors unauthorised-error-container">
+            <div class="page-errors unauthorised-error-header">
+                <h1>404 Not Found</h1>
+            </div>
+            <div class="page-errors unauthorised-error-content">
+                <h2>Reason: </h2>
+                <p>{{$exception->getMessage()}}</p>
+                <h2>What can I do?</h2>
+                <p>Try the following links below: </p>
+                <ul>
+                    <li>To login <a href="{{route('loginPage')}}">Click here.</a></li>
+                    <li>To return to the homepage <a href="{{route('home')}}">Click here</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@endsection
