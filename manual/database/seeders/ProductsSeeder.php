8036958e4bd97101dc115bb4ea4a89cf1baa04d1
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-03-18 20:16:36 +0000

    Added additional car images and more cars to seeder.

--- manual/database/seeders/ProductsSeeder.php
+++ manual/database/seeders/ProductsSeeder.php
@@ -33,14 +33,31 @@ class ProductsSeeder extends Seeder
         $product4 = new Product(3, "535i", "BMW", "A luxury 2010 BMW 535i petrol car.", 9000, 3.5, "Manual", 12);
         $product4->setImagePath("2010_bmw_f10_535i.jpg");
 
-        $product5 = new Product(4, "535i", "BMW", "A luxury 2010 BMW 535i petrol car.", 9000, 3.5, "Manual", 12);
-        $product5->setImagePath("2010_bmw_f10_535i.jpg");
+        $product5 = new Product(4, "Phantom", "Rolls-Royce", "2019 Rolls Royce Phantom Eight Generation Luxury Car V12 Engine", 450000, 6.75, "Automatic", 20);
+        $product5->setImagePath("2019_Rolls-Royce_Phantom_V12_Automatic_6.75.jpg");
+
+        $product6 = new Product(5, "G-Wagon", "Mercedes", "2018 Mercedes G-Wagon G63 AMG", 140000.00, 3.00, "Automatic", 20);
+        $product6->setImagePath("mercedes_g_wagon_g63_amg.jpg");
+
+        $product7 = new Product(6, "C63", "Mercedes", "2020 Mercedes C63 AMG Black Automatic Car", 45000.00, 4.00, "Automatic", 20);
+        $product7->setImagePath("mercedes_c63_amg_coupe.jpg");
+
+        $product8 = new Product(7, "Golf-GTI-6-Speed-Manual", "Volkswagen", "2022 Volkswagen Golf GTI 2.0 Litre Engine Manual Transmission", 36800.00, 2.00, "Manual", 20);
+        $product8->setImagePath("vw_golf_2022_gti.jpg");
+
+        $product9 = new Product(8, "Golf-GTI-Auto", "Volkswagen", "2022 Volkswagen Golf GTI 2.0 Litre Engine Automatic Transmission", 36800.00, 2.00, "Automatic", 20);
+        $product9->setImagePath("vw_golf_2022_gti_auto.jpg");
 
         $carSeeds = array();
         array_push($carSeeds, $product1);
         array_push($carSeeds, $product2);
         array_push($carSeeds, $product3);
         array_push($carSeeds, $product4);
+        array_push($carSeeds, $product5);
+        array_push($carSeeds, $product6);
+        array_push($carSeeds, $product7);
+        array_push($carSeeds, $product8);
+        array_push($carSeeds, $product9);
 
         foreach ($carSeeds as $carSeed) {
             DB::table('products')->insert([
