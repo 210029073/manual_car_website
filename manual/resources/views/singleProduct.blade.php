c26ef48d18edcc2b2ad77e804883807a6020fd16
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-03-24 13:00:10 +0000

    Changed product description heading to paragraph

--- manual/resources/views/singleProduct.blade.php
+++ manual/resources/views/singleProduct.blade.php
@@ -34,7 +34,8 @@
                     {{-- Try not to remove the following lines below, holds backend (Ibrahim)--}}
                     {{-- Will attach to a HTML Form --}}
                     {{-- This should enumerate all product details here... --}}
-                    <h2>{{ $product->description }}</h2>
+                    <p><strong>Product Description:</strong></p>
+                    <p>{{ $product->description }}</p>
                     <p><strong>Engine Capacity:</strong> {{$product->engine_capacity}} litres</p>
                     <p><strong>Transmission:</strong> {{$product->transmission}}</p>
                     {{-- Parse price --}}
