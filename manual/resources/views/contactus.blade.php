e142c560ba1a7b0e43c6494a6aa98bf1ed845d9b
Ahsaan Kumar <ahsaankumar@outlook.de>
2023-03-26 19:05:29 +0100

    HTML & CSS for Contact Us Page.

--- manual/resources/views/contactus.blade.php
+++ manual/resources/views/contactus.blade.php
@@ -9,35 +9,34 @@
         <link rel="stylesheet" href="/css/contactus.css" type="text/css">
     </head>
     <body>
-        <div class="topbackimage">
-            <!-- <img src="/images/.png" position="left:50px;"> -->
-        </div>
-<div class="AboutUsTEST">
-    <div class="boxer">
-        <div class="h4box"><h1>Contact us Via:</h1>
-        <h4>E-Mail: 200070104@aston.ac.uk</h4>
-        <h4>Number: +44 7474 142552</h4>
-        <h4>Location: Aston St, Birmingham B4 7ET </h4></div>
-        <div class="img-container">
-            </div>
-        </div>
-        
-        <div class="AboutUsTEST">
-            <div class="boxer">
-                <div class="h4box"><h1>Contact us Via:</h1>
-                <h4>E-Mail: 200070104@aston.ac.uk</h4>
-                <h4>Number: +44 7474 142552</h4>
-                <h4>Location: Aston St, Birmingham B4 7ET </h4></div>
-                <div class="img-container">
-                    <div class="boxer">
-                        <div class="h4box"><h1>Contact us Via:</h1>
-                        <h4>E-Mail: 200070104@aston.ac.uk</h4>
-                        <h4>Number: +44 7474 142552</h4>
-                        <h4>Location: Aston St, Birmingham B4 7ET </h4></div>
-        <div class="img-container">
-</div>
-</div>
+        <!-- <div class="boxer">
+        <a href="/images/Manual.png"></a>
+</div> -->
 
-</body>
-</html>
-@endsection
+            <!-- <img class="pageImage" src="/images/contactusBig.png" alt=""> -->
+
+        <div class="box-container">
+        <div class="boxer" class="firstboxer">
+            <h2>Reach out to us:</h2>
+            <h3>E-Mail: 200070104@aston.ac.uk</h3>
+            <h3>GitHub Repository: <a href="https://github.com/210029073/manual_car_website/"> Click Here!</a>  </h3>
+            </div>
+
+            <div class="boxer">
+                <img class ="location" src="/images/OurLocation.png" alt="">
+            
+                </div>
+
+
+                    <div class="boxer" class="firstboxer">
+                        <h2>Come And Visit Us:</h2>
+                        <h3>Aston St, Birmingham B4 7ET</h3>
+                       </div>
+    
+
+
+
+        </div>
+    </body>
+    </html>
+    @endsection
